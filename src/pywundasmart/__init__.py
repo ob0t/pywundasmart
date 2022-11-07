96d78f33c5030fabcab1996bee4c181bed1a1a14
Andy Furnell <andy@pod4.org>
2022-11-07 11:58:03 +0000

    Create new function to discover controller details via cloud API

--- src/pywundasmart/__init__.py
+++ src/pywundasmart/__init__.py
@@ -1,1 +1,1 @@
-from .functions import get_devices, put_state
+from .functions import get_devices, put_state, get_credentials
--- src/pywundasmart/functions.py
+++ src/pywundasmart/functions.py
@@ -1,8 +1,13 @@
 import asyncio
+import time
 import aiohttp
 import xmltodict
 import re
 import copy
+import hmac
+import hashlib
+import xmltodict
+import base64
 
 DEVICE_DEFS = ['device_sn', 'prod_sn', 'device_name', 'device_type', 'eth_mac', 't', 'name', 'id', 'type', 'i']
 
@@ -80,11 +85,6 @@ async def get_devices(httpsession, wunda_ip, wunda_user, wunda_pass):
     except (asyncio.TimeoutError, aiohttp.ClientError):
         return {"state": False, "code": 500}
 
-    #devices_list = []
-
-    #for device in devices:
-    #    devices_list.append(devices[device])
-
     return {"state": True, "devices": devices}
 
 async def put_state(httpsession, wunda_ip, wunda_user, wunda_pass, wunda_id, wunda_key, wunda_val):
@@ -113,3 +113,52 @@ async def put_state(httpsession, wunda_ip, wunda_user, wunda_pass, wunda_id, wun
         return {"state": False, "code": 500, "message": "HTTP client error"}
 
     return {"state": True, "response": response}
+
+async def get_credentials(wunda_user, wunda_pass):
+    response = {}
+    wunda_session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=True))
+    curtime = str(round(time.time())+3600)
+    salt = "43785hf284hdf3"
+    key = salt+wunda_pass
+    keyHmacSha512 = hmac.new(hashlib.sha512(str(key).encode()).digest(), curtime.encode(), hashlib.sha512).hexdigest()
+    url = "https://wunda.azurewebsites.net/api/1.1/?login"
+    formdata = aiohttp.FormData( {'user': wunda_user, 'hash': keyHmacSha512, 'time': curtime} )
+    resp = await wunda_session.post(url, data=formdata)
+    status = resp.status
+    if status == 200:
+        if resp.headers["api-status"] == 'ok':
+            resp = await wunda_session.post("https://wunda.azurewebsites.net/api/1.1/?devicelist")
+            if status == 200:
+                if resp.headers["api-status"] == 'ok':
+                    xml_data = await resp.text()
+                    xml_dict = xmltodict.parse(xml_data, attr_prefix='')
+                    device_id = xml_dict["api"]["response"]["device"]["id"]
+                    resp = await wunda_session.post("https://wunda.azurewebsites.net/api/1.1/?id=5056&query=getregister.cgi?0%40auth_root%260%40eth_ip_ro")
+                    if status == 200:
+                        if resp.headers["api-status"] == 'ok':
+                            xml_data = await resp.text()
+                            xml_dict = xmltodict.parse(xml_data, attr_prefix='')
+                            for tid in xml_dict["cmd"]["device"]["reg"]:
+                                response[tid["tid"]] = tid["v"]
+                            (local_user,local_pass) = str(base64.b64decode(response["auth_root"])).split(":")
+                            local_pass = local_pass[:-1]
+                            local_user = local_user[2:]
+                        else:
+                            return {"state": False, "code": 500, "message": resp.headers["api-status-msg"]}
+                    else:
+                        return {"state": False, "code": status}
+                else:
+                    return {"state": False, "code": 500, "message": resp.headers["api-status-msg"]}
+            else:
+                return {"state": False, "code": status}
+        else:
+            return {"state": False, "code": 500, "message": resp.headers["api-status-msg"]}
+    else:
+        return {"state": False, "code": status}
+
+    await wunda_session.close()
+
+    response["user"] = local_user
+    response["pass"] = local_pass
+
+    return response
